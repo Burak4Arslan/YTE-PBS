9099f3312486cee1d466faf22af23d2778095c3b
Burak Arslan <burak4arslan@gmail.com>
2026-07-03 16:14:39 +0300

    init

---
+++ src/test/java/com/yte/pbs/PbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yte.pbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
