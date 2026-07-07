a45a3ac4d47a180fd1b79767a1f6c319737b00d4
avsarece <eceavsar@hotmail.com>
2026-07-07 10:48:36 +0300

    Initialize data for rehber page

--- src/main/java/com/yte/pbs/config/DataInitializer.java
+++ src/main/java/com/yte/pbs/config/DataInitializer.java
@@ -1,8 +1,10 @@
 package com.yte.pbs.config;
 
 import com.yte.pbs.entity.Authority;
+import com.yte.pbs.entity.DirectoryEntry;
 import com.yte.pbs.entity.User;
 import com.yte.pbs.repository.AuthorityRepository;
+import com.yte.pbs.repository.DirectoryEntryRepository;
 import com.yte.pbs.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +18,7 @@ public class DataInitializer {
     CommandLineRunner initializeAuthorityAndUserData(
             AuthorityRepository authorityRepository,
             UserRepository userRepository,
+            DirectoryEntryRepository directoryEntryRepository,
             PasswordEncoder passwordEncoder) {
         return args -> {
             Authority adminAuthority = findOrCreateAuthority(
@@ -41,6 +44,9 @@ public class DataInitializer {
                     "Demir",
                     "personel@pbs.com",
                     employeeAuthority);
+
+            // Initialize directory entries
+            initializeDirectoryEntries(directoryEntryRepository);
         };
     }
 
@@ -70,5 +76,33 @@ public class DataInitializer {
         }
         user.getAuthorities().add(authority);
         userRepository.save(user);
+    }
+
+    private void initializeDirectoryEntries(DirectoryEntryRepository directoryEntryRepository) {
+        directoryEntryRepository.deleteAll();
+        directoryEntryRepository.save(new DirectoryEntry(
+                "Ahmet Yılmaz", "Yazılım Geliştirme", "Kıdemli Mühendis",
+                "Backend Geliştirme", "PBS", "ahmet.yilmaz@yte.org", "0532 111 2233"));
+        directoryEntryRepository.save(new DirectoryEntry(
+                "Fatima Demir", "Yazılım Geliştirme", "Mühendis",
+                "Frontend Geliştirme", "PBS", "fatima.demir@yte.org", "0532 111 2234"));
+        directoryEntryRepository.save(new DirectoryEntry(
+                "Mehmet Kaya", "Sistem Yönetimi", "Sistem Yöneticisi",
+                "Veritabanı Yönetimi", "İnfrastruktur", "mehmet.kaya@yte.org", "0532 111 2235"));
+        directoryEntryRepository.save(new DirectoryEntry(
+                "Ayşe Şimşek", "Yönetim", "Müdür",
+                "Proje Yönetimi", "PBS", "ayse.simsek@yte.org", "0532 111 2236"));
+        directoryEntryRepository.save(new DirectoryEntry(
+                "Elif Özkan", "QA", "Test Mühendisi",
+                "Kalite Kontrol", "PBS", "elif.ozkan@yte.org", "0532 111 2237"));
+        directoryEntryRepository.save(new DirectoryEntry(
+                "Cem Aydın", "Yazılım Geliştirme", "Mühendis",
+                "API Geliştirme", "PBS", "cem.aydin@yte.org", "0532 111 2238"));
+        directoryEntryRepository.save(new DirectoryEntry(
+                "Zeynep Koç", "UI/UX", "Tasarımcı",
+                "Arayüz Tasarımı", "PBS", "zeynep.koc@yte.org", "0532 111 2239"));
+        directoryEntryRepository.save(new DirectoryEntry(
+                "İbrahim Tunç", "DevOps", "Mühendis",
+                "Sunucu Yönetimi", "İnfrastruktur", "ibrahim.tunc@yte.org", "0532 111 2240"));
     }
 }
