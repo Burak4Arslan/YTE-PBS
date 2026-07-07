8757b711a9b0d6f768876672a41e89bff4be9e5b
beyzasuzcn <abeyzasuozcan@gmail.com>
2026-07-07 10:23:04 +0300

    axios ayarları teke düşürme

--- client/app/api/axiosInstance.js
+++ client/app/api/axiosInstance.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
     timeout: 10000,
+    withCredentials: true,
+    xsrfCookieName: 'XSRF-TOKEN',
+    xsrfHeaderName: 'X-XSRF-TOKEN'
 });
 
 export default axiosInstance;
